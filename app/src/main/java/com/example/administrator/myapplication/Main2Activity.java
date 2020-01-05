faf7ddf6238e04b7f587a5c85fcd1a891b31cf70
bing180 <1950482341@qq.com>
2020-01-05 22:16:42 +0800

    566655-oppp

--- app/src/main/java/com/example/administrator/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/administrator/myapplication/Main2Activity.java
@@ -14,5 +14,6 @@ public class Main2Activity extends AppCompatActivity {
                 vdvffffffffffffffffffffffffffffffffffffffffffff
                         ppppppppppppppp
                         ppppppppp
+                                ppppppp
     }
 }
