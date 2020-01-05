78be321c5ab3ead5538b8ccaac60acbe47ba9466
bing180 <1950482341@qq.com>
2020-01-05 22:11:12 +0800

    4444-444

---
+++ app/src/main/java/com/example/administrator/myapplication/Main33Activity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main33Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main33);
+        1111111111111111111111111
+                2222222222222222222222222
+    }
+}
