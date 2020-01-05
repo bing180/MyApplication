d2a9237ec0a56459cd0f954763ac2c58934692d3
bing180 <1950482341@qq.com>
2020-01-05 21:52:32 +0800

    3a.2

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -20,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
                 3.
                         4.
+
                                 5. 3a.1
+                                5.
+                                        66
+
     }
 }
