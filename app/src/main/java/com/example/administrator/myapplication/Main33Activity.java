2825b5379b867ca66d1bfc20ec9367d3096fb83d
bing180 <1950482341@qq.com>
2020-01-05 22:11:33 +0800

    5555---555 mm

--- app/src/main/java/com/example/administrator/myapplication/Main33Activity.java
+++ app/src/main/java/com/example/administrator/myapplication/Main33Activity.java
@@ -11,5 +11,7 @@ public class Main33Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main33);
         1111111111111111111111111
                 2222222222222222222222222
+                        33333333333333
+                                44444444444
     }
 }
